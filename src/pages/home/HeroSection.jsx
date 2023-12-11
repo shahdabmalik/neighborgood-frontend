0b42e1d431958117631b27fa6312a9e43f3767b4
shahdabmalik <shahdab888@gmail.com>
2023-12-11 11:41:23 +0530

    optimize hero image

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import heroImage from "../../assets/heroImage.png"
+import heroImage from "../../assets/heroImage.webp"
 
 const HeroSection = () => {
     return (
