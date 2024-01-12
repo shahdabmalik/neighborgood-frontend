89813b4e8e93776cdff415f509ff6de0e8637a28
shahdabmalik <shahdab888@gmail.com>
2024-01-13 01:24:59 +0530

    using react-facebook-login

--- src/pages/getStarted/GetStarted.jsx
+++ src/pages/getStarted/GetStarted.jsx
@@ -2,37 +2,17 @@ import Navbar from "../../components/navbar/Navbar"
 import getStartedImage from "../../assets/form.webp"
 import { MdEmail } from "react-icons/md";
 import { Link } from "react-router-dom"
-import useFacebookSdk from "../../customHooks/useFacebookSdk";
+import FacebookLogin from 'react-facebook-login';
 
 const GetStarted = () => {
 
     document.title = "Get Started - Neighborgood"
     const appId = import.meta.env.VITE_FB_APPID
 
-    const isSdkReady = useFacebookSdk(appId)
-
-    const handleLogin = () => {
-        if (!isSdkReady) {
-            console.error('Facebook SDK not initialized yet.');
-            return;
-        }
-        window.FB.login(response => {
-            console.log(response);
-            if (response.authResponse) {
-                console.log('Successfully logged in with Facebook!');
-                fetchUserProfile()
-                // Now you can redirect the user or do an API call to your backend with the user's token
-            } else {
-                console.log('User did not fully authorize.');
-            }
-        }, { scope: 'email,public_profile' });
-    };
-
-    const fetchUserProfile = () => {
-        window.FB.api('/me', { fields: 'name,email,picture' }, function (response) {
-            console.log('User profile data:', response);
-            // Handle the user's profile data here
-        });
+    const responseFacebook = (response) => {
+        console.log(response);
+        // Handle the response
+        // The response object will contain user data and the access token
     }
 
     return (
@@ -40,12 +20,19 @@ const GetStarted = () => {
             <Navbar>
             </Navbar>
             <div className="max-w-screen-2xl mx-auto min-h-screen flex flex-col items-center justify-center px-4 md:px-10" >
-                <h1 className="text-3xl md:text-4xl font-semibold text-primary mb-5" >Welcome to Neighborgood!</h1>
+                <h1 className="text-3xl  md:text-4xl font-semibold text-primary mb-5" >Welcome to Neighborgood!</h1>
                 <div className="flex justify-center w-full ">
                     <div className="max-w-[400px] h-[450px] w-full p-4 rounded-md md:rounded-e-none flex flex-col gap-4 shadow-xl border dark:bg-slate-900 border-primary overflow-hidden justify-center" >
                         <Link to={"/login"} className="bg-primary-light text-black flex gap-2 items-center justify-center rounded h-10 font-medium hover:bg-primary-light/75 dark:hover:bg-primary" > <MdEmail size={18} /> Login with email</Link>
                         <Link to={"/regiter"} className="bg-primary-light text-black flex gap-2 items-center justify-center rounded h-10 font-medium hover:bg-primary-light/75 dark:hover:bg-primary" > <MdEmail size={18} /> SignUp with email</Link>
-                        <button onClick={handleLogin} type="button" className="bg-primary-light text-black flex gap-2 items-center justify-center rounded h-10 font-medium hover:bg-primary-light/75 dark:hover:bg-primary" > <MdEmail size={18} /> Login with Facebook</button>
+                        {/* <button onClick={handleLogin} type="button" className="bg-primary-light text-black flex gap-2 items-center justify-center rounded h-10 font-medium hover:bg-primary-light/75 dark:hover:bg-primary" > <MdEmail size={18} /> Login with Facebook</button> */}
+                        <FacebookLogin
+                            appId={appId} // Replace with your Facebook App ID
+                            autoLoad={true}
+                            fields="name,email,picture"
+                            callback={responseFacebook}
+                            icon="fa-facebook" // Optional: customize the button icon
+                        />
                     </div>
                     <div className=" border border-s-0 border-primary hidden rounded-e-md max-w-md w-full shadow-xl lg:flex items-center justify-center">
                         <img className=" w-full object-cover " src={getStartedImage} alt="login" />
